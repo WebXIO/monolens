02a2c3ad2b09f40903b7339e378f14fbedc513eb
Kilian J. Halwachs <kilian.halwachs@stud.hcw.ac.at>
2026-02-17 22:12:04 +0100

    Revert "tbd: added stronghold"
    This reverts commit 8b54cd31ec2a79a80111cd48c7cc74df643dc42d.
    Stronghold is not needed anymore therefore we need to remove it

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,16 +31,9 @@ pub fn run() {
                 .build(),
         )
         .setup(|app| {
-            let salt_path = app
-                .path()
-                .app_local_data_dir()
-                .expect("could not resolve app local data path")
-                .join("salt.txt");
-
             let path = app.path().app_config_dir().unwrap();
-            let repository = FileRepository::new(path);
 
-            app.handle().plugin(tauri_plugin_stronghold::Builder::with_argon2(&salt_path).build())?;
+            let repository = FileRepository::new(path);
 
             let state = create_app_state(repository);
             app.manage(state);
